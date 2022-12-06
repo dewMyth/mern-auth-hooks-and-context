fc69f4f7f63ea02e1cc935127eefe2adf545a1b0
Dewmith Akalanka <akalankadewmith@gmail.com>
2022-12-06 13:37:12 +0530

    Add more validations

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,15 +1,103 @@
 const User = require("../models/userModel");
 
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const validator = require("validator");
+
 // Login User
 // @route POST api/users/login
 const loginUser = async (req, res) => {
-  res.json({ msg: "Login User" });
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return res.status(400).json({
+      msg: "Please enter all fields",
+    });
+  }
+
+  if (!validator.isEmail(email)) {
+    return res.status(400).json({
+      msg: "Invalid email",
+    });
+  }
+
+  const user = await User.findOne({
+    email: email,
+  });
+
+  if (!user) {
+    return res.status(400).json({
+      msg: "User does not exist",
+    });
+  } else {
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      return res.status(400).json({
+        msg: "Invalid credentials",
+      });
+    } else {
+      const token = jwt.sign(
+        {
+          id: user._id,
+          email: user.email,
+        },
+        process.env.JWT_SECRET,
+        {
+          expiresIn: 3600,
+        }
+      );
+
+      res.status(200).json({
+        msg: "Login successful",
+        user: user,
+        token: token,
+      });
+    }
+  }
 };
 
 // Register User
 // @route POST api/users/register
 const registerUser = async (req, res) => {
-  res.json({ msg: "Register User" });
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return res.status(400).json({
+      msg: "Please enter all fields",
+    });
+  }
+
+  if (!validator.isEmail(email)) {
+    return res.status(400).json({
+      msg: "Invalid email",
+    });
+  }
+
+  const isUserExist = await User.findOne({
+    email: email,
+  });
+
+  if (isUserExist) {
+    return res.status(400).json({
+      msg: "User already exist",
+    });
+  } else {
+    const salt = await bcrypt.genSalt(10);
+
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const user = new User({
+      email: email,
+      password: hashedPassword,
+    });
+
+    await user.save();
+    res.status(200).json({
+      msg: "User created successfully",
+      user: user,
+    });
+  }
 };
 
 module.exports = {
--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@ require("dotenv").config();
 
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
+
 const workoutRoutes = require("./routes/workouts");
 const userRoutes = require("./routes/users");
 
@@ -10,6 +12,8 @@ const app = express();
 
 // middleware
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
 app.use((req, res, next) => {
   console.log(req.path, req.method);
