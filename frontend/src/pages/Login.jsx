d0025f1c77aa532897a843f2d1c6e14490a2b1f2
Dewmith Akalanka <akalankadewmith@gmail.com>
2022-12-06 14:11:49 +0530

    Add UIs

---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+
+export const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log(email, password);
+  };
+
+  return (
+    <>
+      <form className="login" onSubmit={handleSubmit}>
+        <h3>Login</h3>
+        <hr />
+        <label htmlFor="email">Email</label>
+        <input
+          type="email"
+          id="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          id="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        <button type="submit">Login</button>
+      </form>
+    </>
+  );
+};
---
+++ frontend/src/pages/Signup.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+
+export const Signup = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log(email, password);
+  };
+
+  return (
+    <>
+      <form className="signup" onSubmit={handleSubmit}>
+        <h3>Sign Up</h3>
+        <hr />
+        <label htmlFor="email">Email</label>
+        <input
+          type="email"
+          id="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          id="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        <button type="submit">Sign Up</button>
+      </form>
+    </>
+  );
+};
