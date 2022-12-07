561c6f2e7be8a9cad9eefd834b7ec39f98871b35
Dewmith Akalanka <akalankadewmith@gmail.com>
2022-12-07 10:34:55 +0530

    Add other api call protections

--- frontend/src/components/WorkoutForm.js
+++ frontend/src/components/WorkoutForm.js
@@ -1,76 +1,102 @@
-import { useState } from "react"
-import { useWorkoutsContext } from "../hooks/useWorkoutsContext"
+import { useState } from "react";
+import { useWorkoutsContext } from "../hooks/useWorkoutsContext";
+import { useAuthContext } from "../hooks/useAuthContext";
 
 const WorkoutForm = () => {
-  const { dispatch } = useWorkoutsContext()
+  const { dispatch } = useWorkoutsContext();
 
-  const [title, setTitle] = useState('')
-  const [load, setLoad] = useState('')
-  const [reps, setReps] = useState('')
-  const [error, setError] = useState(null)
-  const [emptyFields, setEmptyFields] = useState([])
+  const [title, setTitle] = useState("");
+  const [load, setLoad] = useState("");
+  const [reps, setReps] = useState("");
+  const [error, setError] = useState(null);
+  const [emptyFields, setEmptyFields] = useState([]);
+
+  const { user } = useAuthContext();
 
   const handleSubmit = async (e) => {
-    e.preventDefault()
+    e.preventDefault();
 
-    const workout = {title, load, reps}
+    const workout = { title, load, reps };
 
-    const response = await fetch('/api/workouts', {
-      method: 'POST',
+    //validations
+    if (!title || !load || !reps) {
+      setError("Please fill in all fields");
+      return;
+    }
+
+    if (isNaN(load) || isNaN(reps)) {
+      setError("Please enter a number");
+      return;
+    }
+
+    if (load < 0 || reps < 0) {
+      setError("Please enter a real value");
+      return;
+    }
+
+    if (!user) {
+      setError("Please login to add a workout");
+      return;
+    }
+    //end validations
+
+    const response = await fetch("/api/workouts", {
+      method: "POST",
       body: JSON.stringify(workout),
       headers: {
-        'Content-Type': 'application/json'
-      }
-    })
-    const json = await response.json()
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${user.token}`,
+      },
+    });
+    const json = await response.json();
 
     if (!response.ok) {
-      setError(json.error)
-      setEmptyFields(json.emptyFields)
+      setError(json.error);
+      setEmptyFields(json.emptyFields);
     }
     if (response.ok) {
-      setTitle('')
-      setLoad('')
-      setReps('')
-      setError(null)
-      setEmptyFields([])
-      console.log('new workout added', json)
-      dispatch({type: 'CREATE_WORKOUT', payload: json})
+      setTitle("");
+      setLoad("");
+      setReps("");
+      setError(null);
+      setEmptyFields([]);
+      console.log("new workout added", json);
+      dispatch({ type: "CREATE_WORKOUT", payload: json });
     }
-  }
+  };
 
   return (
     <form className="create" onSubmit={handleSubmit}>
       <h3>Add a New Workout</h3>
 
       <label>Excersize Title:</label>
-      <input 
+      <input
         type="text"
         onChange={(e) => setTitle(e.target.value)}
         value={title}
-        className={emptyFields.includes('title') ? 'error' : ''}
+        className={emptyFields.includes("title") ? "error" : ""}
       />
 
       <label>Load (in kg):</label>
-      <input 
+      <input
         type="number"
         onChange={(e) => setLoad(e.target.value)}
         value={load}
-        className={emptyFields.includes('load') ? 'error' : ''}
+        className={emptyFields.includes("load") ? "error" : ""}
       />
 
       <label>Reps:</label>
-      <input 
+      <input
         type="number"
         onChange={(e) => setReps(e.target.value)}
         value={reps}
-        className={emptyFields.includes('reps') ? 'error' : ''}
+        className={emptyFields.includes("reps") ? "error" : ""}
       />
 
       <button>Add Workout</button>
       {error && <div className="error">{error}</div>}
     </form>
-  )
-}
+  );
+};
 
-export default WorkoutForm
+export default WorkoutForm;
